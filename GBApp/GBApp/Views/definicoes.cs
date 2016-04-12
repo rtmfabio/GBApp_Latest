56f08dfc007b1fc5b2bfd54708e50b4072dbf628
fabio mendonca <fabio mendonca@PC-FMENDONCA>
2016-04-12 11:56:17 +0100

    Add project files.

---
+++ GBApp/GBApp/Views/definicoes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace GBApp.Views
+{
+    public class definicoes : ContentPage
+    {
+        public definicoes()
+        {
+            Title = "Definições";
+            BackgroundColor = Color.Navy;
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Página definicoes"
+           
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ GBApp/GBApp/Views/pesquisa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace GBApp.Views
+{
+    public class pesquisa : ContentPage
+    {
+        public pesquisa()
+        {
+            Title = "Pesquisa";
+            BackgroundColor = Color.Aqua;
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label {
+                        Text = "Página de Pesquisa"
+                        
+                    }
+                }
+            };
+        }
+    }
+}
