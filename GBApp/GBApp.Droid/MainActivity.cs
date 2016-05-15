22c66b06f013f7283471e494b015cdb4dcf776a0
Fábio Mendonça <rtm.fabio@gmail.com>
2016-05-15 20:31:21 +0100

    Firt Commit

--- GBApp/GBApp.Droid/MainActivity.cs
+++ GBApp/GBApp.Droid/MainActivity.cs
@@ -17,6 +17,7 @@ namespace GBApp.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.FormsMaps.Init(this, bundle);
             LoadApplication(new App());
         }
     }
--- GBApp/GBApp/App.cs
+++ GBApp/GBApp/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GBApp.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,10 @@ namespace GBApp
         public App()
         {
             // The root page of your application
-            MainPage = new Tabs();
-            
-        }
+            //MainPage = new Tabs();
+            MainPage = new Login();
 
+        }
         protected override void OnStart()
         {
             // Handle when your app starts
